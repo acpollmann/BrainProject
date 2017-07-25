16d730d8bac935f519766d0efe6d0f7b30fb4b13
Robert Ross <demouser@MacBook-Pro.local>
2017-07-25 09:58:59 -0400

    Nope

--- FinalBrain/source/data.py
+++ FinalBrain/source/data.py
@@ -3,6 +3,3 @@ from google.appengine.ext import ndb
 class Data(ndb.Model):
     brain_part = ndb.StringProperty()
     brain_description = ndb.StringProperty()
-
-brain_data = Data(brain_part ='temporal lobe','This is the temporal lobe')
-brain_content = brain_data.put()
